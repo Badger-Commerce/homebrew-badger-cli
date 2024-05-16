bb6ee9d4ef224aa70560b92a8c978747413ccdbe
Chris Kellet <chris.kellet@kedos.co.uk>
2024-05-16 13:18:58 +0100

    Wrong file

--- badger.rb
+++ badger.rb
@@ -3,8 +3,8 @@ class Badger < Formula
 
   desc "A CLI tool for Badger Commerce"
   homepage "https://github.com/Badger-Commerce/badger-cli"
-  url "https://files.pythonhosted.org/packages/d2/7a/d5f811d1e1b0b209d4c7770f585531d2e822ba480d2daf35a1a2f0a973bc/badger_cli-0.1.0-py3-none-any.whl"
-  sha256 "e813495d46ec5d06e844c42f0bfbdd9b9cc4f96ae1323fa96add89253e4f6998"
+  url "https://files.pythonhosted.org/packages/0f/f9/3d314f3bdee7a517b294ad546be64f29a15d1f26fa613a0b5afe7815729a/badger_cli-0.1.0.tar.gz"
+  sha256 "4d3c332568bea774a265c44ca9f01acc6429d064a5179c9298e66e56567d66ae"
   license "MIT"
 
   depends_on "python@3.9"
